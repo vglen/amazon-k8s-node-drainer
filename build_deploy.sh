85af01765b1e5b476e0c20a2d0781c1dba0da1c1
Glen Stummer <glen_stummer@trimble.com>
2019-11-27 11:50:41 -0600

    added region

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,8 @@ BUCKET=$1
 ASG=$2
 CLUSTER=$3
 PROFILE=${4:-default}
+REGION=$5
 
 sam build --use-container --skip-pull-image --profile ${PROFILE}
 sam package --s3-bucket ${BUCKET} --output-template-file packaged.yaml --profile ${PROFILE}
-sam deploy --template-file packaged.yaml --stack-name k8s-drainer --capabilities CAPABILITY_IAM --profile ${PROFILE} --parameter-overrides AutoScalingGroup=${ASG} EksCluster=${CLUSTER}
+sam deploy --template-file packaged.yaml --stack-name k8s-drainer --capabilities CAPABILITY_IAM --profile ${PROFILE} --region ${REGION} --parameter-overrides AutoScalingGroup=${ASG} EksCluster=${CLUSTER}
